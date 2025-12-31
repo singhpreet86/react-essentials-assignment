8430cb99ee6b7397adc9f95b4ab6fe5319252f47
singhpreet86 <33066260+singhpreet86@users.noreply.github.com>
2025-12-31 17:32:01 +0530

    Update Skills.js

--- src/components/Assignment1/PortfolioApp/Skills.js
+++ src/components/Assignment1/PortfolioApp/Skills.js
@@ -1,6 +1,6 @@
-   function Skills({skills}) {
+   function Skills({skills, theme}) {
         return  (
-            <div className="skills-section">
+            <div className={`skills-section ${theme}`}>
                 <span className="skills-label">Skills:</span>
                 {skills.map((skill, index) => 
                     <span key={index} className="skill-item">{skill}</span>
