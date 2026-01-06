d0b8a0c3768725e799c63939881957d0368e4a57
Harpreet Singh <harpreet.7976@gmail.com>
2026-01-06 23:46:07 +0530

    Added form

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,8 +1,61 @@
+import { useState } from "react"
+
 const TaskForm = () => {
+
+    const [FormData, SetFormData] = useState({
+        title: "",
+        description: "",
+        priority: "medium",
+        completed: false
+    })
+
+    const handleSubmit = (e) => {
+        alert("form submit");
+    }
+
+    const handleChange = (e) => {
+        alert("item changed");
+    }
 
     return (
         <div>
-            Form
+            <form onClick={handleSubmit}>
+                <h2> Add Task </h2>
+                <label>Title</label>
+                <input
+                    type="text"
+                    name="title"
+                    value={FormData.title}
+                    onChange={handleChange}
+                    placeholder="Task title....."
+                    required
+                />
+
+                <label>Description</label>
+                <textarea
+                    name="description"
+                    value={FormData.description}
+                    onChange={handleChange}
+                    placeholder="Task description......"
+                    rows="3"
+                />
+
+                <label>Priority</label>
+                <select
+                    name="priority"
+                    value={FormData.priority}
+                    onChange={handleChange}
+                >
+                    <option value="low">Low</option>
+                    <option value="medium">Medium</option>
+                    <option value="high">High</option>
+                </select>
+
+                <button type="submit" disabled={!FormData.title.trim()}>
+                    Add Task
+                </button>    
+
+            </form>
         </div>
     )
 
