2800c8fdbe85167cb7d0e6622a2d7dff60848668
singhpreet86 <33066260+singhpreet86@users.noreply.github.com>
2026-01-09 18:00:30 +0530

    Update WeatherForm.js

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -1,18 +1,16 @@
 import { useState } from "react";
 
-const WeatherForm = ({ setCity, setInputCity }) => {
+const WeatherForm = ({ setCity }) => {
   const [input, setInput] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setCity(input);
     setInput("");
-    setInputCity(true);
   };
 
   const handleChange = (e) => {
     setInput(e.target.value);
-    setInputCity(false);
   }
 
 
