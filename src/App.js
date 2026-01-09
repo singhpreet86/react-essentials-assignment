e8d009a1ea9108321e32e93df85c0b9415c6b3e4
singhpreet86 <33066260+singhpreet86@users.noreply.github.com>
2026-01-09 16:50:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,81 +1,75 @@
-import React , {useState, useEffect} from "react";
+import './App.css';
+import WeatherForm from './components/WeatherForm';
+import WeatherInfo from './components/WeatherInfo';
+import { useState, useEffect } from 'react';
 
 function App() {
 
-  const [user, setUser] = useState(null);
+  const API_KEY = "8d1d55987e65fd7428680f71ad1ecb43";
+
+  const [city, setCity] = useState("Mohali");
+  const [weather, setWeather] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [secondsOnPage, setSecondsOnPage] = useState(0);
-  
+  const [currentCityWeather, setCurrentCityWeather] = useState(null);
+  const [inputCity, setInputCity] = useState(false);
 
-  const fetchUser = async () => {
-  try{
-    const response = await fetch('https://jsonplaceholder.typicode.com/users/4');
-    if(!response.ok){
-      throw new Error(`Http Error! status ${response.status}`);
-    }
-
-    const userData = await response.json();
-    setUser(userData);
-    setLoading(false);
-  }
-  catch(err){
-    setError(err.message);
-    setLoading(false);
-  }
-}
-
-useEffect(() => {
-  fetchUser();
-},[]);
-
-useEffect(() => {
-  if(user){
-    document.title = `Profile: ${user.name} | Uer Dashboard`;
-  }else{
-    document.title = 'Loading............ | User Dashboard';
-  }
-},[user]);
-
-useEffect(() => {
-  const timer = setInterval(() =>{
-    setSecondsOnPage(prev => prev+1);
-  },1000);
-
-  return () => {
-    clearInterval(timer);
+  const fetchWeather =  async () => { 
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      if (!response.ok) {
+        throw new Error(`Http Error! status ${response.status}`);
+      }
+      const data = await response.json();
+      setWeather(data);
+      if (city === "Mohali"){
+      setCurrentCityWeather(data);
+      }
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
+      setInputCity(false);
+    } 
   };
 
-},[]);
+  useEffect(() => {
+    fetchWeather();
+  }, [city]); 
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCity(prev => prev);
+    }, 10000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+
   
   return (
     <div className="App">
-      <h1>User Profile Dashboard</h1>
-      <p style={{textAlign: 'center', color: '#666', marginBottom: '20px'}}>
-        Time on page: {secondsOnPage} seconds
-      </p>
-
-      {error && (
-        <div style={{color: 'red', padding: '20px'}}> 
-            <h2>Error</h2>
-              <p>{error}</p>
+      <div className="header">
+        <h1> Mastering Weather Data With React  </h1>
+      </div>
+      <div className="content">
+        <div className="side-content">
+          <h2>Current Weather in Mohali</h2>
+          {currentCityWeather && <WeatherInfo weather={currentCityWeather} />}          
         </div>
-      )}
+        <div className="main-content">
+          <WeatherForm setCity={setCity} setInputCity={setInputCity} />    
 
-      {loading && !error && (
-        <div style={{padding: '20px', textAlign: 'center'}}>
-          <p> Loading.................</p>
-          </div>
-      )}
-
-      {user && !loading && !error && (
-        <div style={{padding: '20px'}}>
-          <h2> User Information</h2>
-          <p> {user.name} </p>
-          <p> {user.email} </p>
-          <p> {user.phone} </p>
+          {loading && <p>Loading weather data...</p>}
+          {error && <p className="error">{error}</p>}
+          
+          {weather && inputCity && <WeatherInfo weather={weather} />}   
         </div>
-      )}
+      </div>
     </div>
   );
 }
