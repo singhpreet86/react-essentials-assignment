29ba55cec0a04bb78395559eb3db3a899319dda3
singhpreet86 <33066260+singhpreet86@users.noreply.github.com>
2026-01-31 10:22:21 +0530

    Create useExpenses.js

---
+++ src/hooks/useExpenses.js
@@ -0,0 +1,45 @@
+import { useMemo } from "react";
+import useLocalStorage from "./useLocalStorage";
+
+const useExpenses = () => {
+
+     const[expences, setExpences] = useLocalStorage('expenses', []);
+
+
+     const addExpense = (expenseData) => {
+
+        const newExpense = {
+            id: Date.now(),
+            ...expenseData,
+            date: new Date().toISOString().split('T')[0]
+        };
+
+        setExpences(prevExpenses => [newExpense, ...prevExpenses]);
+
+     }
+
+     const removeExpense = (id) => {
+        setExpences(prevExpenses => prevExpenses.filter(expences => expences.id !== id))
+
+     }
+    
+     const getTotalAmount = useMemo(() => {
+        return expences.reduce((sum,expences) => sum+expences.amount,0);
+     }, [expences]);
+
+     const getExpensesByCategory = (category) => {
+        if(!category || category === 'all' ) return expences;
+            return expences.filter(expences => expences.category === category);
+     }
+
+     return{
+        expences,
+        addExpense,
+        removeExpense,
+        getTotalAmount,
+        getExpensesByCategory
+     }
+ 
+}
+
+export default useExpenses
