34cf7a68d6eb001f97b4b29be0d780cb5698ceb9
singhpreet86 <33066260+singhpreet86@users.noreply.github.com>
2026-01-31 16:39:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -102,6 +102,7 @@ function App() {
         <ExpenseList
           filteredExpenses={sortedExpenses}
           removeExpense={removeExpense} 
+          getTotalAmount={getTotalAmount}
           />  
       </section>
      
