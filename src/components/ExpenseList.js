62f26f41a79833cb8896545080bf108b89e0b054
singhpreet86 <33066260+singhpreet86@users.noreply.github.com>
2026-01-31 13:18:14 +0530

    Create ExpenseList.js

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const ExpenseList = ({ filteredExpenses, removeExpense }) => {
+    return (
+         <div className='expense-list'>
+        {filteredExpenses.length === 0 ? (
+          <p style={{ textAlign: 'center', color: '#666', fontStyle: 'italic', margin: '20px' }}>
+            {
+              filteredExpenses.length > 0 ?
+                "No Expense yet, Add your first expense above!" :
+                "No expenses match the current filters. Try adjusting your filter criteria."
+            }
+          </p>
+
+        ) : (
+          filteredExpenses.map(expense => (
+            <div key={expense.id} className='expense-item'>
+              <div className='expense-info'>
+                <div className='expense-description'>{expense.description} </div>
+                <div className='expense-category'>{expense.category} </div>
+                <div style={{ color: '#666', fontSize: '14px' }}>{expense.date} </div>
+              </div>
+
+              <div className='expense-amount'> ${expense.amount.toFixed(2)} </div>
+              <button onClick={() => removeExpense(expense.id)}
+                style={{ background: '#e53e3e', padding: '5px 10px', fontSize: '12px' }}>Delete</button>
+            </div>
+          ))
+        )}
+      </div>
+    );
+};
+
+export default ExpenseList;
