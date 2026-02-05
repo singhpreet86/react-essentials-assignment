a84f38d45aa6595ba6d02900aa44801b8a55e05a
singhpreet86 <33066260+singhpreet86@users.noreply.github.com>
2026-02-05 18:19:54 +0530

    Update Summary.js

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -5,12 +5,8 @@ const Summary = ({ getFilterSummary, getTotalAmount, filteredExpenses, getMonthl
     <div className='total-section'>
       <div className="total-expences">
       <h2> Total Expenses </h2>
-      <div className='total-amount'> ${getTotalAmount.toFixed(2)}</div>
-      {getFilterSummary.hasActiveFilters && (
-        <div style={{ fontSize: '16px', color: '#EEE', marginTop: '10px' }}>
-          (Filtered Total: ${filteredExpenses.reduce((sum, expense) => sum + expense.amount, 0).toFixed(2)})
-        </div>
-      )}
+      <div> <strong>${getTotalAmount.toFixed(2)}</strong></div>
+     
       </div>
       
       <div className="monthly-summary" >
